19bfe241d844bfae5a575b9590b288bbadb64b66
Avinash Bhojane <140709275+avinashbhojane123@users.noreply.github.com>
2026-06-04 23:29:33 +0530

    Update server to use environment port and listen on all interfaces
    Refactor bootstrap function to use environment port and enable listening on all interfaces.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,3 +1,36 @@
+// import { ValidationPipe } from '@nestjs/common';
+// import { NestFactory } from '@nestjs/core';
+
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+
+//   app.enableCors({
+//     origin: '*',
+//     methods: '*',
+//     credentials: true,
+//   });
+
+//   app.useGlobalPipes(
+//     new ValidationPipe({
+//       whitelist: true,
+//       transform: true,
+//       forbidNonWhitelisted: true,
+//     }),
+//   );
+
+//   app.setGlobalPrefix('api');
+
+//   await app.listen(3000);
+
+//   console.log(
+//     `Application running on: http://localhost:3000/api`,
+//   );
+// }
+
+// bootstrap();
+
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
@@ -22,10 +55,12 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api');
 
-  await app.listen(3000);
+  const port = Number(process.env.PORT) || 3000;
+
+  await app.listen(port, '0.0.0.0');
 
   console.log(
-    `Application running on: http://localhost:3000/api`,
+    `Application running on port ${port}`,
   );
 }
 
