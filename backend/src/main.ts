90531c4099943cdd1c100030b479c0750d722ec8
Avinash Bhojane <140709275+avinashbhojane123@users.noreply.github.com>
2026-06-16 12:05:13 +0530

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -34,7 +34,8 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api');
 
-  await app.listen(3000);
+  // await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 
   console.log(
     'Application running on: http://localhost:3000/api',
