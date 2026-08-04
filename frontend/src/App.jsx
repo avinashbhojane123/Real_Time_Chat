f2eafb3f6f5daa364c1b3c1e8b73ad23aed6fbc3
avinashbhojane123 <avinashbhojane123@gmail.com>
2026-08-04 15:26:37 +0530

    Fix SPA client-side 404 routing with HashRouter and historyApiFallback

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import JoinRoom from './pages/JoinRoom.jsx';
 import ChatRoom from './pages/ChatRoom.jsx';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<JoinRoom />} />
         <Route path="/chat" element={<ChatRoom />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
